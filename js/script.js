80496f88906b8a544117302bbba7affd4e2fc4f9
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-10-11 19:40:59 +0200

    Find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -25,9 +25,11 @@ const titleClickHandler = function(event){
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log('articleSelector:', articleSelector)
-    
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.(articleSelector)
+    console.log('targetArticle:', targetArticle);
+
     /* add class 'active' to the correct article */
   }
   

0e831003708d87dac96800ed0bd39eb7ec1b92bc
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-10-14 15:25:40 +0200

    Add ESLint and other required files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 {
-'use strict'
+  'use strict';
 
-const titleClickHandler = function(event){
+  const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
@@ -10,7 +10,7 @@ const titleClickHandler = function(event){
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+    for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
       console.log(activeLink);
     }
@@ -22,7 +22,7 @@ const titleClickHandler = function(event){
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
-    for(let activeArticle of activeArticles){
+    for (let activeArticle of activeArticles) {
       activeArticle.classList.remove('active');
       console.log(activeArticle);
     }
@@ -37,7 +37,7 @@ const titleClickHandler = function(event){
 
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
-  }
+  };
   
   /* const links = document.querySelectorAll('.titles a');
   
@@ -50,7 +50,7 @@ const titleClickHandler = function(event){
     optTitleListSelector = '.titles';
 
   function generateTitleLinks(){
-  console.log(generateTitleLinks)
+    console.log(generateTitleLinks);
 
     /* [DONE] remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
@@ -60,7 +60,7 @@ const titleClickHandler = function(event){
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
 
-    for(let article of articles){
+    for (let article of articles) {
       console.log(article);
 
       /* [DONE] get the article id */
@@ -78,17 +78,17 @@ const titleClickHandler = function(event){
 
       /* [DONE] insert link into titleList */
       html = html + linkHTML;
-  }
+    }
 
-  titleList.innerHTML = html;
-  console.log(html);
+    titleList.innerHTML = html;
+    console.log(html);
 
-  const links = document.querySelectorAll('.titles a');
-  console.log(links);
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
   
   } 
   generateTitleLinks();
